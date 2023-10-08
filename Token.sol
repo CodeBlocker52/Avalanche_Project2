423fc3994bd7fc3701c880001b8104ce1b3a003f
Kundan <131183962+CodeBlocker52@users.noreply.github.com>
2023-10-08 16:47:47 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,68 +1,56 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
-contract MyToken {
-    string public name = "My Token";
-    string public symbol = "MTK";
-    uint8 public decimals = 18;
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
     uint256 public totalSupply;
-    address public owner;
-
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
+    string public name = "MY Token";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+    address public owner;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    constructor(uint256 initialSupply) {
-        totalSupply = initialSupply*10**uint256(decimals) ;
-        balanceOf[msg.sender] = totalSupply;
-        owner = msg.sender;
-    }
-
-    function transfer(address to, uint256 value) public returns (bool) {
-        require(to != address(0), "Invalid recipient");
-        require(balanceOf[msg.sender] >= value, "Insufficient balance");
-
-        balanceOf[msg.sender] -= value;
-        balanceOf[to] += value;
-
-        emit Transfer(msg.sender, to, value);
-        return true;
-    }
-
-    function approve(address spender, uint256 value) public returns (bool) {
-        allowance[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 value) public returns (bool) {
-        require(from != address(0), "Invalid sender");
-        require(to != address(0), "Invalid recipient");
-        require(balanceOf[from] >= value, "Insufficient balance");
-        require(allowance[from][msg.sender] >= value, "Allowance exceeded");
-
-        balanceOf[from] -= value;
-        balanceOf[to] += value;
-        allowance[from][msg.sender] -= value;
-
-        emit Transfer(from, to, value);
-        return true;
-    }
-
-    function mint(address account, uint256 amount) public {
-        require(msg.sender == owner, "Only the owner can mint");
-        require(account != address(0), "Invalid address");
-        totalSupply += amount;
-        balanceOf[account] += amount;
-        emit Transfer(address(0), account, amount);
-    }
-
-    function burn(uint256 amount) public {
-        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
-        totalSupply -= amount;
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool)
+    {
         balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint256 amount) external {
+        require(msg.sender == owner, "Only the owner can mint");
+
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
         emit Transfer(msg.sender, address(0), amount);
     }
 }
